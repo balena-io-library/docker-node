e546b91bba66472a711b0290dbf292a7496814ce
James <james@resin.io>
2015-02-14 17:33:11 +0000

    Dockerfiles added!

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -10,26 +10,26 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 versions=( */ )
 versions=( "${versions[@]%/}" )
-url='git://github.com/resin-io-library/docker-node'
+url='git://github.com/nghiant2710/docker-node'
 
 echo '# maintainer: Joyent Image Team <image-team@joyent.com> (@joyent)'
 echo '# maintainer: Trong Nghia Nguyen - resin.io <james@resin.io>'
 
 for version in "${versions[@]}"; do
-	cd version
+	cd $version
 	fullVersions=( */ )
 	fullVersions=( "${fullVersions[@]%/}" )
 	for fullVersion in "${fullVersions[@]}"; do
 		commit="$(git log -1 --format='format:%H' -- "$fullVersion")"
-		versionAliases=( $fullVersion ${aliases[$version]} )
+		versionAliases=( $fullVersion ${aliases[$fullVersion]} )
 		
-		echo
+		echo 
 		for va in "${versionAliases[@]}"; do
 			echo "$va: ${url}@${commit} $version/$fullVersion"
 		done
 		
 		for variant in onbuild slim wheezy; do
-			commit="$(git log -1 --format='format:%H' -- "$version/$fullVersion/$variant")"
+			commit="$(git log -1 --format='format:%H' -- "$fullVersion/$variant")"
 			echo
 			for va in "${versionAliases[@]}"; do
 				if [ "$va" = 'latest' ]; then
