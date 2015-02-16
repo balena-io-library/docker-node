56a450a70334a2df09f9ed980ce386623dc73e97
nghiant2710 <nghiant2710@users.noreply.github.com>
2015-02-16 19:10:58 +0000

    Update generate-stackbrew-library.sh

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -3,37 +3,43 @@ set -e
 
 declare -A aliases
 aliases=(
-	[0.12]='0.12 latest' [0.8]='0.8' [0.9]='0.9' [0.10]='0.10' [0.11]='0.11'
+	[0.12.0]='0.12 latest' [0.10.36]='0.10' [0.11.16]='0.11' [0.9.12]='0.9' [0.8.28]='0.8'
 )
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 versions=( */ )
 versions=( "${versions[@]%/}" )
-url='git://github.com/joyent/docker-node'
+url='git://github.com/resin-io-library/docker-node'
 
 echo '# maintainer: Joyent Image Team <image-team@joyent.com> (@joyent)'
+echo '# maintainer: Trong Nghia Nguyen - resin.io <james@resin.io>'
 
 for version in "${versions[@]}"; do
-	commit="$(git log -1 --format='format:%H' -- "$version")"
-	fullVersion="$(grep -m1 'ENV NODE_VERSION ' "$version/Dockerfile" | cut -d' ' -f3)"
-	versionAliases=( $fullVersion $version ${aliases[$version]} )
-	
-	echo
-	for va in "${versionAliases[@]}"; do
-		echo "$va: ${url}@${commit} $version"
-	done
-	
-	for variant in onbuild slim wheezy; do
-		commit="$(git log -1 --format='format:%H' -- "$version/$variant")"
-		echo
+	cd $version
+	fullVersions=( */ )
+	fullVersions=( "${fullVersions[@]%/}" )
+	for fullVersion in "${fullVersions[@]}"; do
+		commit="$(git log -1 --format='format:%H' -- "$fullVersion")"
+		versionAliases=( $fullVersion ${aliases[$fullVersion]} )
+		
+		echo 
 		for va in "${versionAliases[@]}"; do
-			if [ "$va" = 'latest' ]; then
-				va="$variant"
-			else
-				va="$va-$variant"
-			fi
-			echo "$va: ${url}@${commit} $version/$variant"
+			echo "$va: ${url}@${commit} $version/$fullVersion"
+		done
+		
+		for variant in onbuild slim wheezy; do
+			commit="$(git log -1 --format='format:%H' -- "$fullVersion/$variant")"
+			echo
+			for va in "${versionAliases[@]}"; do
+				if [ "$va" = 'latest' ]; then
+					va="$variant"
+				else
+					va="$va-$variant"
+				fi
+				echo "$va: ${url}@${commit} $version/$fullVersion/$variant"
+			done
 		done
 	done
+	cd ..
 done
