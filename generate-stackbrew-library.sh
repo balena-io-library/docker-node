71b5fff28eda3e3bf8e4ed6c1e18260b1e551965
Trong Nghia Nguyen <nghiant2710@gmail.com>
2016-04-22 15:40:24 +0700

    Add support for Alpine base images

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,47 +1,90 @@
 #!/bin/bash
 set -e
+set -o pipefail
 
-declare -A aliases
-aliases=(
-	[0.12.7]='0 latest'
-)
+# Generate library file.
+# $1 : device type
+# $2 : distribution
+# $3 : variants
+function generate_library(){
+	if [ $2 == 'debian' ]; then
+		lib_name="$1-node"
+	else
+		lib_name="$1-$2-node"
+	fi
+	path="$1/$2"
 
-cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
-
-repos=( "$@" )
-if [ ${#repos[@]} -eq 0 ]; then
-	repos=( */ )
-fi
-repos=( "${repos[@]%/}" )
-
-echo '# maintainer: Joyent Image Team <image-team@joyent.com> (@joyent)'
-echo '# maintainer: Trong Nghia Nguyen - resin.io <james@resin.io>'
-
-for repo in "${repos[@]}"; do
-
-	cd $repo
+	cd $path
 	versions=( */ )
 	versions=( "${versions[@]%/}" )
-	cd ..
-	url='git://github.com/resin-io-library/docker-node'
+	cd -
+
+	url='git://github.com/resin-io-library/base-images'
+	echo '# maintainer: Joyent Image Team <image-team@joyent.com> (@joyent)' > $lib_name
+	echo '# maintainer: Trong Nghia Nguyen - resin.io <james@resin.io>' >> $lib_name
+
 	for version in "${versions[@]}"; do
-		commit="$(git log -1 --format='format:%H' -- "$repo/$version")"
-		fullVersion="$(grep -m1 'ENV NODE_VERSION ' "$repo/$version/Dockerfile" | cut -d' ' -f3)"
-		versionAliases=( $fullVersion $version ${aliases[$fullVersion]} )
+		commit="$(git log -1 --format='format:%H' -- "$path/$version")"
+		fullVersion="$(grep -m1 'ENV NODE_VERSION ' "$path/$version/Dockerfile" | cut -d' ' -f3)"
+		if [ $version == 'default' ]; then
+			versionAliases=( $fullVersion )
+		else
+			versionAliases=( $fullVersion $version ${aliases[$fullVersion]} )
+		fi
 
-		echo
+		if [ $1 == 'raspberrypi' ] && [ $2 == 'debian' ] && [ $version == '0.12' ]; then
+			continue
+		fi
 
-		for variant in slim; do
-			commit="$(git log -1 --format='format:%H' -- "$repo/$version/$variant")"
-			echo
+		echo >> $lib_name
+
+		for variant in $3; do
+			commit="$(git log -1 --format='format:%H' -- "$path/$version/$variant")"
+			echo >> $lib_name
 			for va in "${versionAliases[@]}"; do
 				if [ "$va" = 'latest' ]; then
 					va="$variant"
 				else
 					va="$va-$variant"
 				fi
-				echo "$va: ${url}@${commit} $repo/$version/$variant"
+				echo "$va: ${url}@${commit} $repo/$path/$version/$variant" >> $lib_name
 			done
 		done
+	done
+}
+
+declare -A aliases
+aliases=(
+	[0.12.7]='0 latest'
+)
+
+defaultVersion='0.10.22'
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+repo=${PWD##*/}
+
+devices=( "$@" )
+if [ ${#devices[@]} -eq 0 ]; then
+	devices=( */ )
+fi
+devices=( "${devices[@]%/}" )
+
+for device in "${devices[@]}"; do
+
+	cd $device
+	distros=( */ )
+	distros=( "${distros[@]%/}" )
+	cd ..
+
+	for distro in "${distros[@]}"; do
+		# Debian
+		if [ $distro == 'debian' ]; then
+			generate_library "$device" "$distro" "slim"
+		fi
+		# Alpine
+		if [ $distro == 'alpine' ]; then
+			generate_library "$device" "$distro" "slim"
+		fi
 	done
 done
