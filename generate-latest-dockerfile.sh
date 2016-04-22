473a7c7a54775c39b86a882acfd254605e39c209
Trong Nghia Nguyen <nghiant2710@gmail.com>
2016-04-22 15:48:52 +0700

    Use checksums to verify node binaries

--- generate-latest-dockerfile.sh
+++ generate-latest-dockerfile.sh
@@ -5,6 +5,22 @@ set -o pipefail
 # comparing version: http://stackoverflow.com/questions/16989598/bash-comparing-version-numbers
 function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | tail -n 1)" == "$1"; }
 
+# extract checksum for node binary
+function extract_checksum()
+{
+	# $1: binary type, 0: in-house, 1: official.
+	# $2: node version
+
+	if [ $1 -eq 0 ]; then
+		checksum=$(grep " node-v$2-linux-$binary_arch.tar.gz" SHASUMS256.txt)
+	else
+		curl -SLO "https://nodejs.org/dist/v$2/SHASUMS256.txt.asc" \
+		&& gpg --verify SHASUMS256.txt.asc \
+		&& checksum=$(grep " node-v$2-linux-$binary_arch.tar.gz\$" SHASUMS256.txt.asc) \
+		&& rm -f SHASUMS256.txt.asc
+	fi
+}
+
 # gpg keys listed at https://github.com/nodejs/node
 for key in \
 9554F04D7259F04124DE6B476D5A82AC7E37093B \
@@ -22,7 +38,7 @@ C4F0DFFF4E8C1A8236409D08E73BC641CC11F4C8 \
 done
 
 archs='armv7hf rpi i386 amd64 armel'
-nodeVersions='0.10.44 4.4.2 5.10.0'
+nodeVersions='0.10.40 4.4.1 5.9.0'
 resinUrl="http://resin-packages.s3.amazonaws.com/node/v\$NODE_VERSION/node-v\$NODE_VERSION-linux-#{TARGET_ARCH}.tar.gz"
 nodejsUrl="http://nodejs.org/dist/v\$NODE_VERSION/node-v\$NODE_VERSION-linux-#{TARGET_ARCH}.tar.gz"
 
@@ -71,10 +87,18 @@ for arch in $archs; do
 			fi
 		fi
 
+		# Extract checksum
+		if [ $binary_url == "$nodejsUrl" ]; then
+			extract_checksum 1 $nodeVersion
+		else
+			extract_checksum 0 $nodeVersion
+		fi
+
 		debian_dockerfilePath=$arch/debian/$baseVersion
 		mkdir -p $debian_dockerfilePath/slim
 		sed -e s~#{FROM}~resin/$baseImage:jessie~g \
 			-e s~#{BINARY_URL}~$binary_url~g \
+			-e s~#{CHECKSUM}~"$checksum"~g \
 			-e s~#{NODE_VERSION}~$nodeVersion~g \
 			-e s~#{TARGET_ARCH}~$binary_arch~g Dockerfile.debian.slim.tpl > $debian_dockerfilePath/slim/Dockerfile
 
@@ -105,12 +129,14 @@ for arch in $archs; do
 		if [ $baseVersion == '0.12' ]; then
 			continue
 		fi
+		extract_checksum 0 $nodeVersion
 
 		alpine_dockerfilePath=$arch/alpine/$baseVersion
 		mkdir -p $alpine_dockerfilePath/slim
 		sed -e s~#{FROM}~resin/$baseImage:latest~g \
 			-e s~#{BINARY_URL}~$binary_url~g \
 			-e s~#{NODE_VERSION}~$nodeVersion~g \
+			-e s~#{CHECKSUM}~"$checksum"~g \
 			-e s~#{TARGET_ARCH}~$binary_arch~g Dockerfile.alpine.slim.tpl > $alpine_dockerfilePath/slim/Dockerfile
 	done
 done
